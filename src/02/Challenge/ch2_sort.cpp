d4698f2371b85b15f9573337fffeea4ae288d9bc
Assar Andersson <assarand@gmail.com>
2023-03-03 10:09:22 +0000

    add quick-sort

--- src/02/Challenge/ch2_sort.cpp
+++ src/02/Challenge/ch2_sort.cpp
@@ -14,18 +14,57 @@
 //           arr: A pointer acting as the array to sort.
 //           n: The size of the array.
 // Returns: A boolean value: True on success, false otherwise.
+
+void swap(int *arr, int i, int j) {
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
+
+
+
 bool sort_array(int *arr, int n){
+    if (n <= 1) {
+        return true;
+    }
+    if (n == 2) {
+        if (arr[0] > arr[1]) {
+            swap(arr, 0,1);
+        }
+        return true;
+    }
+    int up = n-1;
+    int down = 1;
+    swap(arr, 0, n/2);
+    std::cout<<std::endl;
+
+    while (down < up){
+        while(down < n && arr[down] < arr[0]) {
+            down++;
+        }
+        while(up > 0 && arr[up] >= arr[0]) {
+            up--;
+        } 
+        if (down < up) {
+            swap(arr, up, down);
+        }
+    }
+    swap(arr, 0, up);
+    std::cout<<std::endl;
+    sort_array(arr, up);
+    sort_array(arr+down, n-down);
+
 
     // Write your code here
 
-    return false;
+    return true;
 }
 
 // Main function
 int main(){
     // The following array will be treated as an array of length len. 
-    const int len = 7; // Don't exceed the length of the array below!
-    int array[] = {2, 9, 4, 3, 5, 1, 6, 8, 0, 7};
+    const int len = 4; // Don't exceed the length of the array below!
+    int array[] = {1, 0, 2, 3 };
 
     // Print the original array
     std::cout << "Original Array: [ ";
